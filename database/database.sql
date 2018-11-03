25dd7a41d68b6c0667f2d92a49978964502063f6
mirkofranco <mirko.franco@icloud.com>
2018-11-03 15:30:49 +0100

    Update database/database.sql
    Aggiunto vincolo di reazione tabella PRODOTTOSTORICO

--- database/database.sql
+++ database/database.sql
@@ -17,13 +17,13 @@ CREATE TABLE PRODOTTO(
 )ENGINE = InnoDB;
 
 CREATE TABLE PRODOTTOSTORICO(
-    IDStoricizzazione varchar(10),
+    IDStoricizzazione integer auto_increment,
     IDProdotto varchar(10),
     Prezzo float(2) not null,
     DataInizio date not null,
     DataFine date not null,
     primary key(IDStoricizzazione, IDProdotto),
-    foreign key (IDProdotto) references PRODOTTO(IDProdotto)
+    foreign key (IDProdotto) references PRODOTTO(IDProdotto) ON DELETE CASCADE
 )ENGINE = InnoDB;
 
 DROP TRIGGER IF EXISTS aggiuntiProdottoStorico;
@@ -34,8 +34,23 @@ CREATE TRIGGER aggiuntiProdottoStorico
 BEFORE UPDATE ON PRODOTTO
 FOR EACH ROW
 BEGIN
-INSERT INTO PRODOTTOSTORICO(IDProdotto, Prezzo, DataInizio, DataFine) VALUES
-(NEW.IDProdotto, OLD.Prezzo, OLD.DataInizio, curdate());
+	IF OLD.Prezzo <> NEW.Prezzo THEN
+		INSERT INTO PRODOTTOSTORICO(IDProdotto, Prezzo, DataInizio, DataFine) VALUES
+		(NEW.IDProdotto, OLD.Prezzo, OLD.DataInizio, curdate());
+	END IF;
 END; $$
 
 DELIMITER ;
+
+INSERT INTO PRODOTTO(IDProdotto, Categoria, Nome, Marca, Prezzo, DataInizio) VALUES ('0000','Mobile', 'Tavolo', 'Apple', 35, 2018-07-12);
+INSERT INTO PRODOTTO(IDProdotto, Categoria, Nome, Marca, Prezzo, DataInizio) VALUES ('0001','Armadio', 'Armadio Rosa', 'Google', 100, 2018-08-10);
+UPDATE PRODOTTO
+SET Nome = 'Armadio Nuovo'
+WHERE IDProdotto = '0001';
+UPDATE PRODOTTO
+SET Prezzo = 110
+WHERE IDProdotto = '0001';
+UPDATE PRODOTTO
+SET Prezzo = 120
+WHERE  IDProdotto = '0001';
+DELETE FROM PRODOTTO WHERE IDProdotto = '0001';
