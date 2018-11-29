ed5e2b1982e619be64071cd2ab8f3478a82f9982
mirkofranco <mirko.franco@icloud.com>
2018-11-29 15:50:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
     $daSostituire = array(
         "Titolo" => "Home - Studio AR",
-        "<a href=\"./index.php\">" => "",
-        "<a href=\"index.php\"><img src=\"./images/logopngB.png\" id=\"logo\" alt=\"Studio Architetti Riuniti\"/></a>" => "<img src=\"./images/logopngB.png\" id=\"logo\" alt=\"Studio Architetti Riuniti\"/>",
+        "<a href=\"./index.php\">" => "<a href=\"./index.php\" class=\"active\">",
+        "<a href=\"index.php\"><img src=\"./images/logopngB.png\" id=\"logo\" alt=\"Studio Architetti Riuniti\"/></a>" => "<img src=\"./images/logopngB.png\" class=\"active\" id=\"logo\" alt=\"Studio Architetti Riuniti\"/>",
         /*"<li xml:lang=\"en\"><a href=\"./index.php\">Home</a></li>" => "<li xml:lang=\"en\">Home</li>"*/
     );
     echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/inizioU.html'));
