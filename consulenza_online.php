73c55f72b91cab9953703ae3593ca83a65457cb3
mirkofranco <mirko.franco@icloud.com>
2018-12-02 12:07:56 +0100

    Controllo mail consulenza con Js

--- consulenza_online.php
+++ consulenza_online.php
@@ -1,38 +1,26 @@
 <?php
-    require_once('./scripts/php/PHPMailer/class.phpmailer.php');
     require_once('./scripts/php/PatternRule.php');
+    require_once('./scripts/php/Util.php');
     $daSostituire = array(
             "Titolo" => "Consulenza Online - AR",
-            "<a href=\"./consulenza_online.php\">" => "<a href=\"./consulenza_online.php\" class=\"active\">"
+            "<a href=\"./consulenza_online.php\">" => "<a href=\"./consulenza_online.php\" class=\"active\">",
+            "<body>" => "<body onload=\"jsAttivo()\">"
     );
     echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/inizioU.html'));
     echo file_get_contents('./static/consulenzaOnline.html');
-
     if(isset($_POST['chiediConsulenza'])){
-        $mail = new MailRule('/^[A-Za-z0-9-_.+%]+@[A-Za-z0-9-.]+.[A-Za-z]{2,4}$/');
-        $mail -> check($_POST['email'], 'email');
-        if($mail -> isValid()){
-            $mail = new PHPMailer();
-            $mail->IsSMTP();
-            $mail->SMTPAuth = true;
-            $mail->SMTPSecure = 'ssl';
-            $mail->Host = "smtp.gmail.com";
-            $mail->Port = 465; // or 587
-            $mail->IsHTML(true);
-            $mail->Username = "barinmonica.66@gmail.com";
-            $mail->Password = "monica66";
-            $mail->AddAddress("barinmonica.66@gmail.com");
-            $mail -> AddReplyTo($_POST['email']);
-            $mail->Subject = $_POST['fcat'];
-            $mail->Body = $_POST['comment'];
-            if(!$mail->Send()) {
-                echo "Mailer Error: " . $mail->ErrorInfo;
-            } else {
-                echo "Message has been sent";
+        if(!isset($_POST['forJs'])){
+            $mail = new MailRule('/^[A-Za-z0-9-_.+%]+@[A-Za-z0-9-.]+.[A-Za-z]{2,4}$/');
+            $mail -> check($_POST['email'], 'email');
+            if($mail -> isValid()){
+                Util::sendMail("barinmonica.66@gmail.com", $_POST['fcat'], $_POST['comment']);
+            }else{
+                echo $mail -> getFirstError();
             }
         }else{
-            echo $mail -> getFirstError();
+            Util::sendMail("barinmonica.66@gmail.com", $_POST['fcat'], $_POST['comment']);
+            echo "Mail has been sent!";
         }
-    }//if
+    }
     echo file_get_contents('./static/fineU.html');
 ?>
