501875b0a8dc4c5c08311b670f70212c13f212f5
mirkofranco <mirko.franco@icloud.com>
2018-12-09 15:51:07 +0100

    Pulito di nuovo

--- contatti.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $daSostituire = array(
-        "Titolo" => "Contatti - Studio AR",
-        "<a href=\"./contatti.php\">" => "<a href=\"./index.php\" class=\"active\">"
-    );
-    echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/inizioU.html'));
-    echo file_get_contents('./static/contatti.html');
-    echo file_get_contents('static/fineU.html');
-?>
