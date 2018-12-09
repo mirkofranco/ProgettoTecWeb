87747f9cae040f040c7eb738921f2984b24e384a
mirkofranco <mirko.franco@icloud.com>
2018-12-09 15:43:40 +0100

    Prova revert

---
+++ scripts/php/Util.php
@@ -0,0 +1,25 @@
+<?php
+    require_once('PHPMailer/class.phpmailer.php');
+    class Util{
+        public static function sendMail($to, $subject, $body){
+            $mail = new PHPMailer();
+            $mail->IsSMTP();
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = 'ssl';
+            $mail->Host = "smtp.gmail.com";
+            $mail->Port = 465; // or 587
+            $mail->IsHTML(true);
+            $mail->Username = "barinmonica.66@gmail.com";
+            $mail->Password = "monica66";
+            $mail->AddAddress("barinmonica.66@gmail.com");
+            $mail -> AddReplyTo($to);
+            $mail->Subject = $subject;
+            $mail->Body = $body;
+            if(!$mail->Send()) {
+                echo "Mailer Error: " . $mail->ErrorInfo;
+            } else {
+                echo "Message has been sent";
+            }
+        }
+    }
+?>
