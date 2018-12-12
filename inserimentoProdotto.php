da146276c8398aa4a4e79c9fc877ae239a266d41
mirkofranco <mirko.franco@icloud.com>
2018-12-12 16:03:48 +0100

    Update inserimentoProdotto.php

--- inserimentoProdotto.php
+++ inserimentoProdotto.php
@@ -1,8 +1,18 @@
 <?php
+    require_once('./scripts/php/connection.php');
+    require_once('./scripts/php/Prodotto.php');
     $daSostituire = array(
             "Titolo" => "Inserimento Prodotti - Studio AR"
     );
     echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/inizioA.html'));
     echo file_get_contents('./static/formInserimentoProdotti.html');
+    if(isset($_POST['inserisciProdotto'])){
+        $connection = new MySqlDatabaseConnection("localhost", "DatabaseTecnologieWeb", "root", "");
+        $connection -> connect();
+        $connection -> insertProdotto(new Prodotto($_POST['idProdotto'], $_POST['nomeCategoria'], $_POST['nomeProdotto'], $_POST['marcaProdotto'], $_POST['prezzoProdotto'], $_POST['dataInizioPrezzo'], 0));
+        $connection -> close();
+        $connection = null;
+        echo "Prodotto inserito";
+    }
     echo file_get_contents('./static/fineA.html');
 ?>
