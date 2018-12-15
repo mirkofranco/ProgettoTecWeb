a0e20e2cba83793f6d0b030ccaa3287fe88a5be9
erclu <30255227+erclu@users.noreply.github.com>
2018-12-15 20:28:36 +0100

    start working on embedded map; fix broken link in contatti.php

--- contatti.php
+++ contatti.php
@@ -1,7 +1,7 @@
 <?php
     $daSostituire = array(
         "Titolo" => "Contatti - Studio AR",
-        "<a href=\"./contatti.php\">" => "<a href=\"./index.php\" class=\"active\">"
+        "<a href=\"./contatti.php\">" => "<a href=\"./contatti.php\" class=\"active\">"
     );
     echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/inizioU.html'));
     echo file_get_contents('./static/contatti.html');
