605ebc5c54372ba86e49e54f4978d0ae9889b474
mirkofranco <mirko.franco@icloud.com>
2018-12-16 11:29:24 +0100

    close #25

--- about_us.php
+++ about_us.php
@@ -3,7 +3,7 @@
         "{{pageTitle}}" => "About Us - Studio AR",
         "{{pageDescription}}"=>"TODO",
         "{{pageKeywords}}"=>"TODO",
-        "<a href=\"./about_us.php\">" => "<a href=\"./about_us.php\" class=\"active\">"
+        "<a href=\"./about_us.php\" lang=\"en\">" => "<a href=\"./about_us.php\" class=\"active\">"
     );
     echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/inizioU.html'));
     echo file_get_contents('./static/about_us.html');
--- index.php
+++ index.php
@@ -3,12 +3,12 @@
         "{{pageTitle}}" => "Home - Studio AR",
         "{{pageDescription}}"=>"TODO",
         "{{pageKeywords}}"=>"TODO",
-        "<a href=\"./index.php\">" => "<a href=\"./index.php\" class=\"active\">",
+        "<a href=\"./index.php\" lang=\"en\">" => "<a href=\"./index.php\" class=\"active\">",
         "<a href=\"index.php\"><img src=\"./images/logopngB.png\" id=\"logo\" alt=\"Studio Architetti Riuniti\"/></a>" => "<img src=\"./images/logopngB.png\" class=\"active\" id=\"logo\" alt=\"Studio Architetti Riuniti\"/>"
         /*"<li xml:lang=\"en\"><a href=\"./index.php\">Home</a></li>" => "<li xml:lang=\"en\">Home</li>"*/
     );
     // FIXME: non funziona la class active su questa pagina
-    
+
     echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/inizioU.html'));
     echo file_get_contents('./static/index.html');
     echo file_get_contents('static/fineU.html');
