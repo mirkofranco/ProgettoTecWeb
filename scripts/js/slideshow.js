765f6a425e8bf93a69a6a0b2ec7ef87604d19b31
erclu <30255227+erclu@users.noreply.github.com>
2018-12-17 14:09:47 +0100

    rinominato selettore my-slides; stile consistente sui commenti; tolta chiamata a script ridondante

--- scripts/js/slideshow.js
+++ scripts/js/slideshow.js
@@ -11,7 +11,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("my-slides");
+  var slides = document.getElementsByClassName("slideshow-element");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
