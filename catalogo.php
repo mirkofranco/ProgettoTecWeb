7a68e8c323939886d0f93c2f0555958bb76afcdd
erclu <30255227+erclu@users.noreply.github.com>
2018-12-19 16:10:58 +0100

    icone header

--- catalogo.php
+++ catalogo.php
@@ -3,7 +3,7 @@
         "{{pageTitle}}" => "Catalogo - Studio AR",
         "{{pageDescription}}"=>"TODO",
         "{{pageKeywords}}"=>"TODO",
-        "<a href=\"./catalogo.php\">" => "<a href=\"#\" class=\"current-page\">"
+        "<a href=\"./catalogo.php\">" => "<a href=\"./catalogo.php\" class=\"current-page\">"
     );
     echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/_inizio_user.html'));
     echo file_get_contents('./static/catalogo.html');
