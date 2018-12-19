d2ca62080447b6df7b4434869ed116e1519f9142
elisabetta-p <elisabetta.piombin@gmail.com>
2018-12-19 15:03:22 +0100

    aggiornati file php
    aggiunto campo descrizione nel file .sql, e modifiche di conseguenza nei file php
    
    Co-Authored-By: Mirko Franco <mirkofranco@users.noreply.github.com>

--- scripts/php/connection.php
+++ scripts/php/connection.php
@@ -26,10 +26,10 @@ class MySqlDatabaseConnection extends AbstractConnection{
 
     /* Inserisce un prodotto nel database */
     public function insertProdotto($prodotto){ //insert vista dal punto di vista del database, non del prodotto: per questo è qui e non in una eventuale classe prodotto
-        $toInsert = "INSERT INTO PRODOTTO(IDProdotto, Categoria, Nome, Marca, Prezzo, DataInizio, isOfferta, NomeImmagine) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+        $toInsert = "INSERT INTO PRODOTTO(IDProdotto, Categoria, Nome, Marca, Prezzo, DataInizio, isOfferta, NomeImmagine, Descrizione) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
         //$this -> connect();
         $stmt = $this -> pdo -> prepare($toInsert);
-        $okai = $stmt -> execute([$prodotto -> getID(), $prodotto -> getCategoria(), $prodotto -> getNome(), $prodotto -> getMarca(), $prodotto -> getPrezzo(), $prodotto -> getDataInizioPrezzo(), $prodotto -> getOfferta(), $prodotto -> getNomeImmagine()]);
+        $okai = $stmt -> execute([$prodotto -> getID(), $prodotto -> getCategoria(), $prodotto -> getNome(), $prodotto -> getMarca(), $prodotto -> getPrezzo(), $prodotto -> getDataInizioPrezzo(), $prodotto -> getOfferta(), $prodotto -> getNomeImmagine(), $prodotto->getDescrizione()]);
         //$this -> close();
         return $okai;
     }
@@ -40,7 +40,7 @@ class MySqlDatabaseConnection extends AbstractConnection{
         $prodotti = $this -> pdo -> query("SELECT * FROM PRODOTTO") -> fetchAll();
         $toReturn = array();
         foreach($prodotti as $row){
-            array_push($toReturn, new Prodotto($row['IDProdotto'], $row['Categoria'], $row['Nome'], $row['Marca'], $row['Prezzo'], $row['DataInizio'], $row['isOfferta']));
+            array_push($toReturn, new Prodotto($row['IDProdotto'], $row['Categoria'], $row['Nome'], $row['Marca'], $row['Prezzo'], $row['DataInizio'], $row['isOfferta'], $row ['Descrizione']));
         }
         return $toReturn;
     }
