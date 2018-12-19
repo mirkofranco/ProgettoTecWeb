fff7eb0c36c20d7ff6da329585ae93d89c0449df
mirkofranco <mirko.franco@icloud.com>
2018-12-19 16:41:50 +0100

    Controlli nuovi consulenza online

--- consulenza_online.php
+++ consulenza_online.php
@@ -1,29 +1,29 @@
 <?php
-    require_once('./scripts/php/PatternRule.php');
     require_once('./scripts/php/Util.php');
-    $daSostituire = array(
-            "{{pageTitle}}" => "Consulenza Online - AR",
-            "{{pageDescription}}"=>"TODO",
-            "{{pageKeywords}}"=>"TODO",
-            "<a href=\"./consulenza_online.php\">" => "<a href=\"./consulenza_online.php\" class=\"current-page\">",
-            "<body>" => "<body onload=\"jsAttivo()\">"
-    );
-    echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/_inizio_user.html')); //cerca il primo parametro, e nel terzo ci
-    //mette quello che trova nel secondo
-    echo file_get_contents('./static/consulenza_online.html');
+    $errorForm = "";
     if(isset($_POST['chiediConsulenza'])){ //si chiede se il bottone è stato cliccato
         if(!isset($_POST['forJs'])){ //se sì => controlla se js è attivo, fino a riga 20 js NON È ATTIVO
-            $mail = new MailRule('/^[A-Za-z0-9-_.+%]+@[A-Za-z0-9-.]+.[A-Za-z]{2,4}$/'); //la mail è nel formato corretto?  (esp reg che definisce il formato della mail)
-            $mail -> check($_POST['email'], 'email'); //controlla che la mail sia nel formato definito prima
-            if($mail -> isValid()){ //se la mail è valida => se l'array è vuoto ritorna true
+            if(/*$mail -> isValid()*/ preg_match('/^[A-Za-z0-9-_.+%]+@[A-Za-z0-9-.]+.[A-Za-z]{2,4}$/', $_POST['email'])){ //se la mail è valida => se l'array è vuoto ritorna true
                 Util::sendMail("barinmonica.66@gmail.com", $_POST['fcat'], $_POST['comment']); //manda la mail
             }else{  //la mail non è valida
-                echo $mail -> getFirstError(); //messaggio di errore: la funzione è dentro una classe in uno dei due file sopra
+                //echo $mail -> getFirstError(); //messaggio di errore: la funzione è dentro una classe in uno dei due file sopra
+                $errorForm = "La mail non è valida";
             }
         }else{ //se js è attivo si entra qua, il controllo sulla mail lo fa js e quindi mando la mail easy
             Util::sendMail("barinmonica.66@gmail.com", $_POST['fcat'], $_POST['comment']);
             echo "Mail has been sent!";
         }
     }
+    $daSostituire = array(
+            "{{pageTitle}}" => "Consulenza Online - AR",
+            "{{pageDescription}}"=>"TODO",
+            "{{pageKeywords}}"=>"TODO",
+            "<a href=\"./consulenza_online.php\">" => "<a href=\"./consulenza_online.php\" class=\"current-page\">",
+            "<body>" => "<body onload=\"jsAttivo()\">",
+            "{{errorForm}}" => $errorForm
+    );
+    echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/_inizio_user.html')); //cerca il primo parametro, e nel terzo ci
+    //mette quello che trova nel secondo
+    echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/consulenza_online.html'));
     echo file_get_contents('./static/_fine.html');
 ?>
