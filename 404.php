3dc2b1caf23164fc8c0adcb5eb7583472e47bdf5
erclu <30255227+erclu@users.noreply.github.com>
2018-12-26 22:40:58 +0100

    microfix su commit precedente

--- 404.php
+++ 404.php
@@ -4,10 +4,9 @@
         "{{pageDescription}}" => "pagina di errore 404",
         "{{pageKeywords}}" => " "
     );
-
-    $customMessage = "<p>Purtroppo l'indirizzo " . $_SERVER["REQUEST_URI"] . " non esiste su questo sito!</p>";
+    $requestedUrl = $_SERVER["REQUEST_URI"];
 
     echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/_inizio_user.html'));
-    echo str_replace("{{requestedUrl}}", $customMessage, file_get_contents('./static/404.html'));
+    echo str_replace("{{requestedUrl}}", $requestedUrl, file_get_contents('./static/404.html'));
     echo file_get_contents('./static/_fine.html');
 ?>
