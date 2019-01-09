d0cfb9ef592c7dc92433e6b3e399ba3091e81c10
Nicoletta <nicolettafbr@gmail.com>
2019-01-09 11:54:56 +0100

    aggiustate pagine admin e inseriti bottoni per il ritorno al pannello admin

--- catalogo_amministratori.php
+++ catalogo_amministratori.php
@@ -5,7 +5,7 @@
         "{{pageKeywords}}"=>"TODO"
     );
     echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/_inizio_user.html'));
-    echo file_get_contents('./static/menu_catalogo.html');
+    /*echo file_get_contents('./static/menu_catalogo.html');*/
     echo file_get_contents('./static/catalogo_amministratori.html');
     echo file_get_contents('./static/_fine.html');
 ?>
