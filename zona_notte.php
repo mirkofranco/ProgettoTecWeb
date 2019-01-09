512c81a009146dd7bd78ba955c826ebf93a1d98a
erclu <30255227+erclu@users.noreply.github.com>
2019-01-09 14:24:57 +0100

    iniziato lavoro pagine sottocategorie

--- zona_notte.php
+++ zona_notte.php
@@ -1,11 +1,32 @@
 <?php
+    echo "NOT WORKING";
+    die();
+    
+    $thisFile = ".".$_SERVER["PHP_SELF"];
+
     $daSostituire = array(
         "{{pageTitle}}" => "Zona notte - Studio AR",
         "{{pageDescription}}"=>"TODO",
         "{{pageKeywords}}"=>"TODO",
+        "{{linkPaginaCategoria}}" => $thisFile
     );
-    echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/_inizio_user.html'));
-    echo file_get_contents('./static/menu_catalogo.html');
-    echo file_get_contents('./static/zona_notte.html');
-    echo file_get_contents('./static/_fine.html');
+
+    $page = file_get_contents('./static/_inizio_user.html').
+            file_get_contents('./static/menu_catalogo.html').
+            file_get_contents('./static/zona_notte.html').
+            file_get_contents('./static/_fine.html');
+
+    $document = new DOMDocument();
+    $document->loadHTML($page);
+            
+    echo $document->saveHTML;
+
+    // $page = str_replace(array_keys($daSostituire), array_values($daSostituire), $page);
+    // echo $page;
+
+
+    // echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/_inizio_user.html'));
+    // echo file_get_contents('./static/menu_catalogo.html');
+    // echo file_get_contents('./static/zona_notte.html');
+    // echo file_get_contents('./static/_fine.html');
 ?>
