9e789cd6c688a0699d89b190b5856540ffbeeaef
elisabetta-p <elisabetta.piombin@gmail.com>
2019-01-10 12:02:14 +0100

    messo il logout in about us. entro oggi pomeriggio lo metto nelle altre pagine!

--- about_us.php
+++ about_us.php
@@ -1,9 +1,19 @@
 <?php
+
+	require_once('./scripts/php/Sessione.php');
+    Sessione::startSession();
+    $gestioneLogin = "";
+    if(!isset($_SESSION['user'])){
+        $gestioneLogin = "<a href=\"index_admin.php\" class=\"header-button\" >Login</a><a href=\"registrazione.php\" class=\"header-button\" >Registrati</a>";
+    }else{
+        $gestioneLogin = "<a href=\"logout.php\" class=\"header-button\">Logout</a>";
+    }
     $daSostituire = array(
         "{{pageTitle}}" => "About Us - Studio AR",
         "{{pageDescription}}"=>"TODO",
         "{{pageKeywords}}"=>"TODO",
-        "<a href=\"./about_us.php\" lang=\"en\">" => "<a href=\"./about_us.php\" class=\"current-page\">"
+        "<a href=\"./about_us.php\" lang=\"en\">" => "<a href=\"./about_us.php\" class=\"current-page\">",
+        "{{gestioneLogin}}" => $gestioneLogin
     );
     echo str_replace(array_keys($daSostituire), array_values($daSostituire), file_get_contents('./static/_inizio_user.html'));
     echo file_get_contents('./static/about_us.html');
