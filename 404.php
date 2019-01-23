f653f24f4625fb0ffe8dbb9883a86162f86ba2cd
erclu <30255227+erclu@users.noreply.github.com>
2019-01-23 17:41:40 +0100

    fix pagina 404

--- 404.php
+++ 404.php
@@ -1,12 +1,12 @@
-<?php	
-  require_once('./scripts/php/Utente.php');
-	require_once('./scripts/php/Sessione.php');
+<?php
+    require_once('./scripts/php/Utente.php');
+    require_once('./scripts/php/Sessione.php');
     Sessione::startSession();
     $gestioneLogin = "";
     if(!isset($_SESSION['user'])){
         $gestioneLogin = "<a href=\"index_admin.php\" class=\"header-button\" >Login</a><a href=\"registrazione.php\" class=\"header-button\" >Registrati</a>";
     }else{
-		if($_SESSION['user'] -> getPermessi() == '11'){
+        if($_SESSION['user'] -> getPermessi() == '11'){
             $gestioneLogin .= "<a href=\"index_admin.php\" class=\"header-button\">Area riservata</a>";
         }
         $gestioneLogin .= "<a href=\"logout.php\" class=\"header-button\">Logout</a>";
