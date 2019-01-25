50bc473a3eddb31b713cf41b37f09cb4a33567d8
erclu <30255227+erclu@users.noreply.github.com>
2019-01-25 17:52:12 +0100

    tolto file inutilizzato piccoli fix a .htaccess formatting su styles

--- scripts/js/menu_laterale.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("sidebar-catalogo").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("sidebar-catalogo").style.width = "0";
-}
