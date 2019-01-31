8331e56dc5743e456e1f6afeb96b12e10c65e3ff
erclu <30255227+erclu@users.noreply.github.com>
2019-01-31 15:48:52 +0100

    fix elimina prodotto

--- elimina_prodotto.php
+++ elimina_prodotto.php
@@ -6,9 +6,7 @@ $result = $connection->deleteProdotto($_GET['id']);
 $connection->close();
 
 if ($result) {
-    echo "il prodotto è stato eliminato correttamente; tra 5 secondi verrai reindirizzato all'area riservata amministratori";
-    sleep(5);
-    header("location: ./catalogo_amministratori.php");
+    header('Location: ./catalogo_amministratori.php');
 } else {
     echo "Qualcosa è andato storto. Contatta il tuo sysadmin di fiducia, oppure <a href=\"./index.php>torna alla home</a>";
 }
