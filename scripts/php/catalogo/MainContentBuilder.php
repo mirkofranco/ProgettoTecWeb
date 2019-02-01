bd5cb5806eacb2ced80b688112fb909bc8f91f49
erclu <30255227+erclu@users.noreply.github.com>
2019-02-01 14:48:19 +0100

    aggiunta doc su costruttore sottopagine catalogo

--- scripts/php/catalogo/MainContentBuilder.php
+++ scripts/php/catalogo/MainContentBuilder.php
@@ -1,46 +1,61 @@
 <?php
-    require_once("SubCategoryBuilder.php");
-    require_once("SidebarBuilder.php");
+require_once "SubCategoryBuilder.php";
+require_once "SidebarBuilder.php";
 
-    class MainContentBuilder {
+/**
+ * costruisce una sottopagina del catalogo
+ */
+class MainContentBuilder {
 
-      private $sidebarBuilder;
-      private $subCategoriesHtml;
+    private $sidebarBuilder;
+    private $subCategoriesHtml;
 
-        public function __construct($currentCategory) {
-          $this->sidebarBuilder = new SidebarBuilder($currentCategory);
-        }
+    public function __construct($currentCategory) {
+        $this->sidebarBuilder = new SidebarBuilder($currentCategory);
+    }
 
-        public function parseCategoriesMap($categoriesMap) {
-          $this->sidebarBuilder->parseCategoriesMap($categoriesMap);
-        }
-        
-        public function parseProductsMap($productsMap) {
-          $this->subCategoriesHtml = array();
+    /**
+     * aggiunge le sottocategorie (indicizzate per categoria) alla sidebar
+     */
+    public function parseCategoriesMap($categoriesMap) {
+        $this->sidebarBuilder->parseCategoriesMap($categoriesMap);
+    }
 
-          foreach ($productsMap as $subCategoryName => $products) {
+    /**
+     * aggiunge i prodotti (indicizzati per sottocategoria) alla pagina
+     */
+    public function parseProductsMap($productsMap) {
+        $this->subCategoriesHtml = array();
+
+        foreach ($productsMap as $subCategoryName => $products) {
 
             $subCategory = new SubCategoryBuilder($subCategoryName);
 
             foreach ($products as $productAttributes) {
                 $subCategory->addNewProduct($productAttributes);
             }
-    
+
             $this->subCategoriesHtml[] = $subCategory->buildHtml();
-          }
-        }
-
-        public function getSidebar() {
-          return $this->sidebarBuilder->buildHtml();
-        }
-
-        public function getProducts() {
-          return join("\n", $this->subCategoriesHtml);
         }
     }
 
-    // <div id="#sottocategoria">
-    //   [VEDI FILE /scripts/php/catalogo/SubCategoryBuilder.php]
-    // </div>
-    // ALTRE SOTTOCATEGORIE......
+    /**
+     * ritorna il codice html della sidebar per questa categoria
+     */
+    public function getSidebar() {
+        return $this->sidebarBuilder->buildHtml();
+    }
+
+    /**
+     * ritorna il codice html della lista di prodotti per questa categoria
+     */
+    public function getProducts() {
+        return join("\n", $this->subCategoriesHtml);
+    }
+}
+
+// <div id="#sottocategoria">
+//   [VEDI FILE /scripts/php/catalogo/SubCategoryBuilder.php]
+// </div>
+// ALTRE SOTTOCATEGORIE......
 ?>
